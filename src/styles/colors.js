f5faa339ece74bb7ff2311bbb7876f52a23acd95
[epserradresi] <epserradresi@gmail.com>
2023-08-18 17:44:16 +0300

    Colors and pages added.

---
+++ src/styles/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+  black: '#000',
+  white: '#fff',
+  lightGray: '#F5F5F5',
+  gray: '#D9D9D9',
+  green: '#2FAD2C',
+  blue: '#0056A5',
+  oceanBlue: '#00948B',
+};
+
+export default colors;
