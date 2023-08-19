56c131f83b6a4625f68f6f92c5d51d3ad69a8d8f
[epserradresi] <epserradresi@gmail.com>
2023-08-19 17:46:36 +0300

    fetchUserLocation function has been added to run in the background with react-native-background-actions.

--- src/utils/fetchUserLocation.js
+++ src/utils/fetchUserLocation.js
@@ -2,13 +2,16 @@ import {useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import Geolocation from '@react-native-community/geolocation';
 import {changeUserLocation} from '../redux/reducers';
+import BackgroundService from 'react-native-background-actions';
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
 
 const FetchUserLocation = () => {
   const userLocation = useSelector(state => state.userLocation);
   const userSession = useSelector(state => state.userSession);
   const dispatch = useDispatch();
 
-  console.log(userLocation);
+  //   console.log(userLocation);
 
   const getLocation = () => {
     Geolocation.getCurrentPosition(
@@ -28,19 +31,42 @@ const FetchUserLocation = () => {
     );
   };
 
+  const veryIntensiveTask = async taskDataArguments => {
+    const {delay} = taskDataArguments;
+    await new Promise(async resolve => {
+      for (let i = 0; BackgroundService.isRunning(); i++) {
+        getLocation();
+        await sleep(delay);
+      }
+    });
+  };
+
+  const startBackgroundTask = async () => {
+    const options = {
+      taskName: 'FetchUserLocationTask',
+      taskTitle: 'Fetching User Location',
+      taskDesc: 'Getting user location every 10 seconds',
+      taskIcon: {
+        name: 'ic_launcher',
+        type: 'mipmap',
+      },
+      color: '#FF00FF',
+      parameters: {
+        delay: 10000,
+      },
+    };
+
+    await BackgroundService.start(veryIntensiveTask, options);
+  };
+
   useEffect(() => {
     if (userSession) {
-      getLocation();
-
-      const intervalId = setInterval(() => {
-        getLocation();
-      }, 10000);
-
-      return () => {
-        clearInterval(intervalId);
-      };
+      startBackgroundTask();
     }
 
+    return () => {
+      BackgroundService.stop();
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userSession]);
 
